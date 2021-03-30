fa518f8b0ceaba8d3917dc211408389322d5315d
Reductions <slavi.boyanov@gmail.com>
2021-03-30 07:01:12 +0300

    Added lint package and fixed warnings

--- test/test_package/lib/lib.dart
+++ test/test_package/lib/lib.dart
@@ -4,21 +4,27 @@ export 'package:test_package/src/src.dart';
 String libFunction() => "libFunction";
 
 class DefaultObject implements SomeObjectBaseClass {
+  @override
   String get id => "default";
 }
 
 class PositionalArgumentsObject implements SomeObjectBaseClass {
   PositionalArgumentsObject(this.id);
+
+  @override
   String id;
 }
 
 class NamedArgumentsObject implements SomeObjectBaseClass {
   NamedArgumentsObject({this.id = ''});
 
+  @override
   String id;
 }
 
 class NamedConstructorObject implements SomeObjectBaseClass {
   NamedConstructorObject.fromID();
+
+  @override
   String get id => "fromID";
 }
